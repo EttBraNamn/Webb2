aa8e7a370867f03145c3fa7062507d2645153df8
EttBraNamn <anton.johansson3@elev.ga.ntig.se>
2019-04-12 13:23:05 +0200

    qwertyuio

--- root/deleteComment.php
+++ root/deleteComment.php
@@ -1,38 +1,73 @@
 <?php
 
+function Error($s)
+{
+    echo($s);
+    exit();
+}
+
 include 'globalVal.php';
 include 'security.php';
 
 session_start();
 
+//Checks if all the required values are defined correctly
+if (!isset($_POST['id']))
+{
+    header("location: index.php");
+    exit();
+}
 if (!isset($_SESSION['name']))
 {
     header("location: index.php");
     exit();
 }
-else if (VerifyUser($_SESSION['name'], $_SESSION['password']) != "correct")
-{
-    header("location: index.php");
-    exit();
-}
-else if (!isset($_POST['date']))
+if (VerifyUser($_SESSION['name'], $_SESSION['password']) != "correct")
 {
     header("location: index.php");
     exit();
 }
 
-$date = $_POST['date'];
-$name = $_SESSION['name'];
+
+
+
 $id = $_POST['id'];
+$name = $_SESSION['name'];
 
+$connect = new PDO("mysql:host=" . SERVERNAME . ";dbname=" . DBNAME, USERNAME, PASSWORD);
+
+$query = "SELECT * FROM post WHERE id=:id AND name=:name";
+
+if (!$stmt = $connect->prepare($query))
+{
+	Error("Couldn't prepare query" . $query);
+}
+$stmt->bindParam(":id", $id);
+$stmt->bindParam(":name", $name);
+//If it can't be executed
+if(!$stmt->execute())
+{
+    Error("Couldn't execure query" . $query);
+}
+
+$ret = $stmt->fetchAll();
+
+//Delets the post with the right id and name. That way nobody can delete something that the user never posted
 if (in_array($name, ADMIN))
 {
-    $query = "DELETE FROM comments WHERE id=:id AND date=:date";
+    
+    $query = "DELETE FROM post WHERE id=:id";
 }
-else 
+else
 {
-    $query = "DELETE FROM comments WHERE id=:id AND date=:date AND name=\"" . $name . "\"";
+	if (empty($ret))
+	{
+		Error("You can't delete other people's posts");
+	}
+    $query = "DELETE FROM post WHERE id=:id AND name=\"" . $name . "\"";
 }
+
+
 $connect = new PDO("mysql:host=" . SERVERNAME . ";dbname=" . DBNAME, USERNAME, PASSWORD);
 //If it can't be prepared
 if (!$stmt = $connect->prepare($query))
@@ -40,14 +75,23 @@ if (!$stmt = $connect->prepare($query))
 	Error("Couldn't prepare query" . $query);
 }
 $stmt->bindParam(":id", $id);
-$stmt->bindParam(":date", $date);
 //If it can't be executed
 if(!$stmt->execute())
 {
-    Error("Couldn't execure query" . $query);
+    Error("Couldn't execute query" . $query);
 }
 
+$query = "DELETE FROM comments WHERE id=:id";
+if (!$stmt = $connect->prepare($query))
+{
+	Error("Couldn't prepare query" . $query);
+}
+$stmt->bindParam(':id', $id);
+if(!$stmt->execute())
+{
+    Error("Couldn't execute query" . $query);
+}
 
-header("location: post.php?id=" . $id);
+header("location: index.php");
 exit();
 ?>
--- root/deletePost.php
+++ root/deletePost.php
@@ -33,6 +33,24 @@ if (VerifyUser($_SESSION['name'], $_SESSION['password']) != "correct")
 
 $id = $_POST['id'];
 $name = $_SESSION['name'];
+
+$connect = new PDO("mysql:host=" . SERVERNAME . ";dbname=" . DBNAME, USERNAME, PASSWORD);
+
+$query = "SELECT * FROM post WHERE id=:id AND name=:name";
+
+if (!$stmt = $connect->prepare($query))
+{
+	Error("Couldn't prepare query" . $query);
+}
+$stmt->bindParam(":id", $id);
+//If it can't be executed
+if(!$stmt->execute())
+{
+    Error("Couldn't execure query" . $query);
+}
+
+$ret = $stmt->fetchAll();
+
 //Delets the post with the right id and name. That way nobody can delete something that the user never posted
 if (in_array($name, ADMIN))
 {
@@ -41,6 +59,10 @@ if (in_array($name, ADMIN))
 }
 else
 {
+	if (empty($ret))
+	{
+		Error("You can't delete other people's posts");
+	}
     $query = "DELETE FROM post WHERE id=:id AND name=\"" . $name . "\"";
 }
 
